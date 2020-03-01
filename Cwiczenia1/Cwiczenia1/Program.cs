f7a80bee94b005f985b00b5d5c4429a34e39515e
Krzysztof Dąbkowski <s17470@pjwstk.edu.pl>
2020-03-01 16:06:34 +0100

    Dodana znaczna liczba kodu :)

--- Cwiczenia1/Cwiczenia1/Program.cs
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Cwiczenia1
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var client = new HttpClient();
+            HttpResponseMessage result = await client.GetAsync("https://www.pja.edu.pl"); //dodajemy "await. Potrzebne zeby program poczekal na zaladowanie stron www. W przeciwnym razie ta linia nie wykona sie. Dodalismy tez w linii klasy slowa async i Task".
+
+            if(result.IsSuccessStatusCode)
+            {
+                string html = await result.Content.ReadAsStringAsync();
+            }
+            
+            Console.WriteLine("Koniec!");
         }
     }
 }
