7d48ea71ca2d79d76033ec7a7066e376a8531899
Krzysztof Dąbkowski <s17470@pjwstk.edu.pl>
2020-03-01 16:12:35 +0100

    Dodana opcja wypisywania maili ze strony pja.edu.pl.

--- Cwiczenia1/Cwiczenia1/Program.cs
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Cwiczenia1
@@ -15,6 +16,15 @@ namespace Cwiczenia1
             if(result.IsSuccessStatusCode)
             {
                 string html = await result.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z0-9]+@[a-z0-9]+\\.+[a-z]+",
+                                        RegexOptions.IgnoreCase);
+                var matches = regex.Matches(html);
+
+                foreach(var m in matches)
+                {
+                    Console.WriteLine(m);
+                }
+
             }
             
             Console.WriteLine("Koniec!");
